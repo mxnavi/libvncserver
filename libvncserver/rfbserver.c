a264f93c3b426115c689ce8c454cb110be52c1c6
yangyz <yangyz@meixing.com>
2019-03-22 15:42:26 +0800

    Add one log to confirm the encode that client uses.

--- libvncserver/rfbserver.c
+++ libvncserver/rfbserver.c
@@ -2419,7 +2419,7 @@ rfbProcessClientNormalMessage(rfbClientPtr cl)
         else
         {
           if (lastPreferredEncoding==-1) {
-              rfbLog("Using %s encoding for client %s\n", encodingName(cl->preferredEncoding,encBuf,sizeof(encBuf)),cl->host);
+              rfbErr("Using %s encoding for client %s\n", encodingName(cl->preferredEncoding,encBuf,sizeof(encBuf)),cl->host);
           } else {
               rfbLog("Switching from %s to %s Encoding for client %s\n", 
                   encodingName(lastPreferredEncoding,encBuf2,sizeof(encBuf2)),
